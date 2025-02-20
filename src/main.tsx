96ab2aaac27f1ff04d8870d7dd5aedcc69a9ac96
ramagdev <ramagtdev@gmail.com>
2025-02-20 08:26:45 +0800

    Testing path fix with hashrouting

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Ganti BrowserRouter ke HashRouter
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import HomePage from './presentation/pages/HomePage';
 import { TambahNasabahPage } from './presentation/pages/TambahNasabahPage';
 import { NotFoundPage } from './presentation/pages/NotFoundPage';
 
-// Ambil base URL dari environment Vite
-const base = import.meta.env.BASE_URL;
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={base}>
+    <HashRouter> {/* Tidak perlu basename */}
       <Routes>
         <Route path="/">
           <Route index element={<App />} />
-          <Route path="#/home" element={<HomePage />} />
-          <Route path="#/tambah-nasabah" element={<TambahNasabahPage />} />
+          <Route path='home' element={<HomePage />} />
+          <Route path="tambah-nasabah" element={<TambahNasabahPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
